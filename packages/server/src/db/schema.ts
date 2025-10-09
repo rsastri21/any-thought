a4b79304a72554e7e39347695211d4350c65bfaf
rsastri21 <rsastri21@gmail.com>
2025-10-08 22:01:39 -0700

    WIP: Add table definitions for friends/friend requests.

--- packages/server/src/db/schema.ts
+++ packages/server/src/db/schema.ts
@@ -1,4 +1,5 @@
-import { pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { pgEnum, pgTable, primaryKey, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: text("id").primaryKey(),
@@ -15,3 +16,75 @@ export const users = pgTable("users", {
     .notNull()
     .defaultNow(),
 });
+
+export const usersRelations = relations(users, ({ many }) => ({
+  friends: many(friends),
+  friendRequests: many(friendRequests),
+}));
+
+export const friends = pgTable(
+  "friends",
+  {
+    userIdLeft: text("user_id_left")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    userIdRight: text("user_id_right")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    createdAt: timestamp("created_at", {
+      withTimezone: true,
+      mode: "date",
+    })
+      .notNull()
+      .defaultNow(),
+  },
+  (t) => [primaryKey({ columns: [t.userIdLeft, t.userIdRight] })],
+);
+
+export const friendsRelations = relations(friends, ({ one }) => ({
+  userLeft: one(users, {
+    fields: [friends.userIdLeft],
+    references: [users.id],
+  }),
+  userRight: one(users, {
+    fields: [friends.userIdRight],
+    references: [users.id],
+  }),
+}));
+
+export const friendRequestStatus = pgEnum("friend_request_status", [
+  "pending",
+  "accepted",
+  "rejected",
+]);
+
+export const friendRequests = pgTable(
+  "friendRequests",
+  {
+    requester: text("requester")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    requestee: text("requestee")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    status: friendRequestStatus(),
+    createdAt: timestamp("created_at", {
+      withTimezone: true,
+      mode: "date",
+    })
+      .notNull()
+      .defaultNow(),
+  },
+  (t) => [primaryKey({ columns: [t.requester, t.requestee] })],
+);
+
+export const friendRequestsRelations = relations(friendRequests, ({ one }) => ({
+  requester: one(users, {
+    fields: [friendRequests.requester],
+    references: [users.id],
+  }),
+  requestee: one(users, {
+    fields: [friendRequests.requestee],
+    references: [users.id],
+  }),
+}));
